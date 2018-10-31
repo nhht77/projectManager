88109f6285eef40e7cb196c05e4ac208a27a2033
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-10-31 12:17:25 +0200

    output projectItems

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
     
     render() {
-        console.log(this.props);
+        let projectItems;
+        if(this.props.projects){
+          projectItems = this.props.projects.map(project => {
+            //console.log(project);
+            return (
+              <ProjectItem  key={project.title} project={project} />
+            );
+          });
+        }
         return (
         <div className="Projects">
-            My Projects
-            {this.props.test}
+            {projectItems}
         </div>
         );
     }
